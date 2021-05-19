b98301d6c4b28e6b40554e6382650ca2964926b2
tyrantgenesis <genesis489449701@gmail.com>
2021-05-19 19:10:20 +0800

    update

--- tyrantgenesis.js
+++ tyrantgenesis.js
@@ -30,5 +30,25 @@ const secParse = _ => {
     let d = [];
     let data = fetch(MY_URL);
 
-    setError(data)
+    data.forEach(item => {
+        d.push({
+            title: item.title,
+            col_type: 'long_text'
+        })
+
+        item.rules.forEach(rule => {
+            d.push({
+                title: rule.title,
+                url: "rule://"+rule.url,
+                col_type: 'text_2'
+            })
+        })
+
+        d.push({
+            col_type: 'line_blank'
+        })
+    })
+
+    res.data = d;
+    setHomeResult(res);
 }
