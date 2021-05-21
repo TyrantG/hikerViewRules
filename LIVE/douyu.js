2172568c9a5dbf56eb2afb8e5f8d02de19db6642
tyrantgenesis <genesis489449701@gmail.com>
2021-05-21 16:34:15 +0800

    add public

--- LIVE/douyu.js
+++ LIVE/douyu.js
@@ -9,7 +9,7 @@ const baseParse = _ => {
 
     d.push({
         desc: '36',
-        url: 'hiker://files/TyrantG/public/douyu-search.html',
+        url: 'file:///storage/emulated/0/Android/data/com.example.hikerview/files/Documents/TyrantG/public/douyu-search.html',
         col_type:"x5_webview_single",
     })
 
