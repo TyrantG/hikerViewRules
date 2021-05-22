4015384460a78bb769cccee3a74337709b12b944
tyrantgenesis <genesis489449701@gmail.com>
2021-05-22 17:33:01 +0800

    God Plan

--- God_Plan.js
+++ God_Plan.js
@@ -7,40 +7,42 @@
 const godWriteFile = (fetch_file, local_file) => {
   let local = request(local_file);
   let fetch = request(fetch_file);
-  if (!local || local != fetch) writeFile(local_file, fetch)
+  if (!local || local !== fetch) writeFile(local_file, fetch)
 }
+
+const time = (new Date()).getTime()
 
 /* Base Function */
 
 /* Customize Function */
 
 const tyrantgenesis = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/base.json", 'hiker://files/TyrantG/data/base.json')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/tyrantgenesis.js", 'hiker://files/TyrantG/tyrantgenesis.js')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/base.json?t="+time, 'hiker://files/TyrantG/data/base.json')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/tyrantgenesis.js?t="+time, 'hiker://files/TyrantG/tyrantgenesis.js')
 }
 
 const douyu_live = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/douyu.js", 'hiker://files/TyrantG/LIVE/douyu.js')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/douyu.js?t="+time, 'hiker://files/TyrantG/LIVE/douyu.js')
 }
 
 const yande_re = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/yande.re.js", 'hiker://files/TyrantG/GHS/yande.re.js')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/yande.re.js?t="+time, 'hiker://files/TyrantG/GHS/yande.re.js')
 }
 
 const pornhub = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/pornhub.js", 'hiker://files/TyrantG/GHS/pornhub.js')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/pornhub.js?t="+time, 'hiker://files/TyrantG/GHS/pornhub.js')
 }
 
 const spankbang = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/spankbang.js", 'hiker://files/TyrantG/GHS/spankbang.js')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/spankbang.js?t="+time, 'hiker://files/TyrantG/GHS/spankbang.js')
 }
 
 const aktool = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/aktool.js", 'hiker://files/TyrantG/TOOL/aktool.js')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/aktool.js?t="+time, 'hiker://files/TyrantG/TOOL/aktool.js')
 }
 
 const prts = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/prts.js", 'hiker://files/TyrantG/TOOL/prts.js')
+  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/prts.js?t="+time, 'hiker://files/TyrantG/TOOL/prts.js')
 }
 
 /* Customize Function */
