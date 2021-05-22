ec3e291dc0a84e2f65437a0f31150bbcedc76bf4
tyrantgenesis <genesis489449701@gmail.com>
2021-05-22 14:34:33 +0800

    God Plan

--- God_Plan.js
+++ God_Plan.js
@@ -4,9 +4,9 @@
 * @param fetch_file String 远程文件web地址
 * @param local_file String 本地文件路径
 * */
-function godWriteFile(fetch_file, local_file) {
-  let local = fetch(local_file);
-  let fetch = fetch(fetch_file);
+const godWriteFile = (fetch_file, local_file) => {
+  let local = request(local_file);
+  let fetch = request(fetch_file);
   if (!local || local != fetch) writeFile(local_file, fetch)
 }
 
@@ -14,12 +14,12 @@ function godWriteFile(fetch_file, local_file) {
 
 /* Customize Function */
 
-function tyrantgenesis() {
+const tyrantgenesis = _ => {
   godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/base.json", 'hiker://files/TyrantG/data/base.json')
   godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/tyrantgenesis.js", 'hiker://files/TyrantG/tyrantgenesis.js')
 }
 
-function douyu_live() {
+const douyu_live = _ => {
   godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/douyu.js", 'hiker://files/TyrantG/LIVE/douyu.js')
 }
 
