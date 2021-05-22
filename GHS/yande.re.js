16e38ce5344129712ad46142d5b2b67705628ecf
tyrantgenesis <genesis489449701@gmail.com>
2021-05-22 16:11:32 +0800

    God Plan

--- GHS/yande.re.js
+++ GHS/yande.re.js
@@ -15,3 +15,34 @@ const baseParse = _ => {
   res.data = d;
   setHomeResult(res);
 }
+
+const secParse = _ => {
+  let res = {};
+  let d = [];
+  const html = getResCode();
+  const desc = parseDomForHtml(html, "#content");
+
+  d.push({
+    pic_url: parseDomForHtml(desc, ".highres-show&&href"),
+    url: parseDomForHtml(desc, ".highres-show&&href")
+  })
+  res.data = d;
+  setHomeResult(res);
+}
+
+const searchParse = _ => {
+  let res = {};
+  let d = [];
+  const html = getResCode();
+  const list = parseDomForArray(html, "#post-list-posts&&li");
+
+  list.forEach(element => {
+    d.push({
+      pic_url: parseDomForHtml(element, ".preview&&src"),
+      url: "https://yande.re"+parseDomForHtml(element, "a&&href")
+    })
+  });
+
+  res.data = d;
+  setHomeResult(res);
+}
