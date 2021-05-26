286f3e936ebb390437407a4fed0eeb85c85c7af5
tyrantgenesis <genesis489449701@gmail.com>
2021-05-27 01:49:04 +0800

    ohentai

--- GHS/ohentai.js
+++ GHS/ohentai.js
@@ -14,31 +14,26 @@ const baseParse = _ => {
 
 const secParse = _ => {
   let d = [];
-  const list = parseDomForArray(getResCode(), '.videobrickwrap&&.videobrick||.videobrick&&.interlink');
+  try{
+    const list = parseDomForArray(getResCode(), '.videobrickwrap&&.videobrick');
 
-  for (let j in list) {
-    let a
-    try {
-      // a = parseDomForHtml(list[0], 'img&&data-cfsrc')
-      a = parseDomForHtml(list[0], 'img&&data-cfsrc')
-    }catch(e) {
-      a = "img/video_placeholder.png"
+    for (let j in list) {
+      d.push({
+        title: parseDomForHtml(list[j], '.videotitle&&Text'),
+        pic_url: "https://ohentai.org/"+parseDomForHtml(list[j], 'img&&src').replace(/\s/g, '%20'),
+        url: $("https://ohentai.org/"+parseDomForHtml(list[j],'a&&href').replace(/\s/g, '%20')).lazyRule(_ => {
+          const data = fetch(input).match(/sources: \[\{\"file\"\:\".*\"\}\],/)
+
+          if (data) {
+            return data[0].replace(/sources: \[\{\"file\"\:\"/, '').replace(/\"\}\],/, '')
+          } else {
+            return "取值失败"
+          }
+        }),
+        col_type: "movie_2"
+      });
     }
-    d.push({
-      title: parseDomForHtml(list[j], '.videotitle&&Text'),
-      pic_url: "https://ohentai.org/"+a.replace(/\s/g, '%20'),
-      url: $("https://ohentai.org/"+parseDomForHtml(list[j],'a&&href').replace(/\s/g, '%20')).lazyRule(_ => {
-        const data = fetch(input).match(/sources: \[\{\"file\"\:\".*\"\}\],/)
-
-        if (data) {
-          return data[0].replace(/sources: \[\{\"file\"\:\"/, '').replace(/\"\}\],/, '')
-        } else {
-          return "取值失败"
-        }
-      }),
-      col_type: "movie_2"
-    });
-  }
+  }catch(e){}
 
   setResult(d);
 }
