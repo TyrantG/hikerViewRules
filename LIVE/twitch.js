7bece14afd8d0606bf30bb64d966ba2c26761601
tyrantgenesis <genesis489449701@gmail.com>
2021-06-01 18:33:28 +0800

    twitch

--- LIVE/twitch.js
+++ LIVE/twitch.js
@@ -35,17 +35,6 @@ const secParse = _ => {
                 const client_id = html.match(/"Client-ID":"(.*?)"/)[1]
                 const rid = MY_URL.split('/').pop()
 
-                /*const data = {
-                    "operationName": "PlaybackAccessToken_Template",
-                    "query": "query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isLive) {    value    signature    __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isVod) {    value    signature    __typename  }}",
-                    "variables": {
-                        "isLive": true,
-                        "login": rid,
-                        "isVod": true,
-                        "vodID": "",
-                        "playerType": "site"
-                    }
-                }*/
                 const data = {
                     "operationName": "PlaybackAccessToken",
                     "extensions": {
@@ -84,18 +73,26 @@ const secParse = _ => {
                 const playlist = fetch(stream_url)
 
                 const lines = playlist.split('\n');
-                d.push({
-                    title: "如果显示“commercial break in progress”请等15秒",
-                    col_type: 'text_center_1'
-                });
-                for (let i = 4; i < lines.length; i += 3) {
+                if (lines.length < 5) {
                     d.push({
-                        title: lines[i - 2].split('NAME="')[1].split('"')[0],
-                        // resolution: (lines[i - 1].indexOf('RESOLUTION') != -1 ? lines[i - 1].split('RESOLUTION=')[1].split(',')[0] : null),
-                        url: lines[i],
-                        col_type: 'text_2'
+                        title: "当前主播没有在直播",
+                        col_type: 'text_center_1'
                     });
+                } else {
+                    d.push({
+                        title: "如果显示“commercial break in progress”请等15秒",
+                        col_type: 'text_center_1'
+                    });
+                    for (let i = 4; i < lines.length; i += 3) {
+                        d.push({
+                            title: lines[i - 2].split('NAME="')[1].split('"')[0],
+                            // resolution: (lines[i - 1].indexOf('RESOLUTION') != -1 ? lines[i - 1].split('RESOLUTION=')[1].split(',')[0] : null),
+                            url: lines[i],
+                            col_type: 'text_2'
+                        });
+                    }
                 }
+
                 setResult(d);
             }),
             col_type: 'movie_2'
@@ -104,44 +101,3 @@ const secParse = _ => {
 
     setResult(d);
 }
-
-/*
-const videoParse = _ => {
-    const header = {'User-Agent': PC_UA}
-    const html = fetch(input, {headers: header})
-    const client_id = html.match(/"Client-ID":"(.*?)"/)[1]
-    const rid = input.split('/').pop()
-
-    const data = {
-        "operationName": "PlaybackAccessToken_Template",
-        "query": "query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isLive) {    value    signature    __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isVod) {    value    signature    __typename  }}",
-        "variables": {
-            "isLive": true,
-            "login": rid,
-            "isVod": true,
-            "vodID": "",
-            "playerType": "site"
-        }
-    }
-
-    const headers = {
-        'Client-ID': client_id,
-        'Content-Type': 'application/json',
-        'Referer': 'https://www.twitch.tv/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.214 Safari/537.36'
-    }
-
-    const data_json = fetch('https://gql.twitch.tv/gql', {headers: headers, method:'POST', body: JSON.stringify(data)})
-    const {value, signature, _} = JSON.parse(data_json).data.streamPlaybackAccessToken
-
-    return 'https://usher.ttvnw.net/api/channel/hls/'+rid+'.m3u8?allow_source=true' +
-        '&dt=2&fast_bread=true' +
-        '&player_backend=mediaplayer' +
-        '&playlist_include_framerate=true' +
-        '&reassignments_supported=true' +
-        '&sig=' + signature +
-        '&supported_codecs=vp09,avc1' +
-        '&token=' + encodeURIComponent(value) +
-        '&cdm=wv' +
-        '&player_version=1.4.0'
-}*/
