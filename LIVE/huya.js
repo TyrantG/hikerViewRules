fb33e2a055c9fd4be4c9ec18cb0c80ecae97191a
tyrantgenesis <genesis489449701@gmail.com>
2021-06-02 23:14:15 +0800

    huya

--- LIVE/huya.js
+++ LIVE/huya.js
@@ -163,9 +163,8 @@ const cateGroupParse = _ => {
     const list_json = parseDomForHtml(getResCode(), "#ssrData&&Text")
 
     const json = JSON.parse(list_json)
-    setError(json.gameList)
 
-    /*const tabCurrent = parseInt(getVar('tab-current') || 0)
+    const tabCurrent = parseInt(getVar('tab-current') || 0)
     switch (tabCurrent) {
         case 1: cate = JSON.parse(fetch("https://live.cdn.huya.com/liveconfig/game/bussLive?bussType=1")).data;break;
         case 2: cate = JSON.parse(fetch("https://live.cdn.huya.com/liveconfig/game/bussLive?bussType=2")).data;break;
@@ -190,7 +189,7 @@ const cateGroupParse = _ => {
             }),
             col_type:"icon_round_4"
         });
-    });*/
+    });
     setResult(d);
 }
 
