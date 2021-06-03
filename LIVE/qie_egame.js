b046fe16a82e9498db6cc0c4a7d614619787fac1
tyrantgenesis <genesis489449701@gmail.com>
2021-06-03 23:08:11 +0800

    qie

--- LIVE/qie_egame.js
+++ LIVE/qie_egame.js
@@ -50,10 +50,26 @@ const cateGroupParse = _ => {
       title: parseDomForHtml(item, 'a&&title'),
       pic_url: parseDomForHtml(item, 'img&&src'),
       url: $(parseDom(item, 'a&&href')+'&_=_').rule(_ => {
+        let d = [];
         const layout_id = MY_URL.match(/layoutid=(.*?)&/)[1]
 
-        const data_json = fetch("https://share.egame.qq.com/cgi-bin/pgg_async_fcgi?param={\"key\":{\"module\":\"pgg_live_read_ifc_mt_svr\",\"method\":\"get_pc_live_list\",\"param\":{\"appid\":\""+layout_id+"\",\"page_num\":2,\"page_size\":40,\"tag_id\":0,\"tag_id_str\":\"\"}}}", {headers:{"User-Agent":PC_UA}})
-        setError(data_json)
+        const data_json = fetch("https://share.egame.qq.com/cgi-bin/pgg_async_fcgi?param={\"key\":{\"module\":\"pgg_live_read_ifc_mt_svr\",\"method\":\"get_pc_live_list\",\"param\":{\"appid\":\""+layout_id+"\",\"page_num\":fypage,\"page_size\":16,\"tag_id\":0,\"tag_id_str\":\"\"}}}", {headers:{"User-Agent":PC_UA}})
+        const list = JSON.parse(data_json).data[0].retBody.data.live_data.live_list
+        list.forEach(item => {
+          let data = JSON.parse(item.data_h5)
+          d.push({
+            title: data.title,
+            desc: data.anchor_name,
+            pic_url: data.program_res.cover_url,
+            col_type: 'movie_2',
+            url: $(data.jump_url).rule(_ => {
+              eval(fetch('hiker://files/TyrantG/LIVE/qie_egame.js'))
+              secParse()
+            })
+          })
+        })
+
+        setResult(d);
       }),
       col_type: 'movie_3',
     })
