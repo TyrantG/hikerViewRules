6d53209c90789d0beaad3e4413b53b0c5fb3e5e0
tyrantgenesis <genesis489449701@gmail.com>
2021-06-03 23:16:03 +0800

    qie

--- LIVE/qie_egame.js
+++ LIVE/qie_egame.js
@@ -53,7 +53,7 @@ const cateGroupParse = _ => {
         let d = [];
         const layout_id = MY_URL.match(/layoutid=(.*?)&/)[1]
 
-        const data_json = fetch("https://share.egame.qq.com/cgi-bin/pgg_kit_async_fcgi", {headers: {"Content-Type": "application/x-www-form-urlencoded"}, method: 'POST', body: "param={\"0\":{\"param\":{\"layout_id\":\""+layout_id+"\",\"page_num\":fypage,\"page_size\":16,\"scene\":0,\"tag_id\":0},\"module\":\"pgg_live_read_svr\",\"method\":\"get_live_list\"}}"})
+        const data_json = fetch("https://share.egame.qq.com/cgi-bin/pgg_kit_async_fcgi", {headers: {"Content-Type": "application/x-www-form-urlencoded","User-Agent":MOBILE_UA}, method: 'POST', body: "param={\"0\":{\"param\":{\"layout_id\":\""+layout_id+"\",\"page_num\":fypage,\"page_size\":16,\"scene\":0,\"tag_id\":0},\"module\":\"pgg_live_read_svr\",\"method\":\"get_live_list\"}}"})
         setError(JSON.parse(data_json))
         /*const list = JSON.parse(data_json).data.key.retBody.data.live_data.live_list
         list.forEach(item => {
