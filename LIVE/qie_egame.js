e25453516764b790458a7eaf8c011c885ed89385
tyrantgenesis <genesis489449701@gmail.com>
2021-06-03 23:20:15 +0800

    qie

--- LIVE/qie_egame.js
+++ LIVE/qie_egame.js
@@ -54,7 +54,7 @@ const cateGroupParse = _ => {
         const layout_id = MY_URL.match(/layoutid=(.*?)&/)[1]
 
         const data_json = fetch("https://share.egame.qq.com/cgi-bin/pgg_kit_async_fcgi", {headers: {"Content-Type": "application/x-www-form-urlencoded","User-Agent":MOBILE_UA,"origin":"https://cdn.egame.qq.com"}, method: 'POST', body: "param={\"0\":{\"param\":{\"layout_id\":\""+layout_id+"\",\"page_num\":fypage,\"page_size\":16,\"scene\":0,\"tag_id\":0},\"module\":\"pgg_live_read_svr\",\"method\":\"get_live_list\"}}"})
-        setError(JSON.parse(data_json))
+        setError(layout_id)
         /*const list = JSON.parse(data_json).data.key.retBody.data.live_data.live_list
         list.forEach(item => {
           let data = JSON.parse(item.data_h5)
