ea3c7933a67d0d07f2cc1dafd90ca648d6846dc6
tyrantgenesis <genesis489449701@gmail.com>
2021-06-04 11:46:37 +0800

    hoem

--- tyrantgenesis.js
+++ tyrantgenesis.js
@@ -72,7 +72,7 @@ const homePage = _ => {
     let d = [];
 
     d.push({
-        desc: '100% && list',
+        desc: '100% && float',
         url: 'file:///storage/emulated/0/Android/data/com.example.hikerview/files/Documents/TyrantG/public/Home.html?time='+(new Date()).getTime(),
         col_type:"x5_webview_single"
     })
