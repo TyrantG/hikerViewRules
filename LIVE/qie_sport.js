6e11629cc851104ca3601278a09a9c589cf7ed62
tyrantgenesis <genesis489449701@gmail.com>
2021-06-06 17:50:41 +0800

    es

--- LIVE/qie_sport.js
+++ LIVE/qie_sport.js
@@ -10,7 +10,7 @@ const baseParse = _ => {
   const child_category_json = fetch(child_cate_url)
   const child_list = JSON.parse(child_category_json).data
 
-  const data_url = "https://live.qq.com/api/live/vlist?page_size=16&page="+page+"&shortName="+category[current].short_name+"&child_id="+child_list[current_child].child_id
+  const data_url = "https://live.qq.com/api/live/vlist?page_size=16&page="+page+"&shortName="+category[current].short_name+"&child_id="+current_child === '' ? '' : child_list[current_child].child_id
   const data_json = fetch(data_url)
   const list = JSON.parse(data_json).data.result
 
