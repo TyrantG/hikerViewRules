6e5b249a2e86f4176c06408883ebafe3725b6669
tyrantgenesis <genesis489449701@gmail.com>
2021-06-24 15:02:19 +0800

    update

--- God_Plan.js
+++ God_Plan.js
@@ -1,3 +1,4 @@
+const BASE_URL = "https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/"
 /* Base Function */
 
 /**
@@ -23,148 +24,153 @@ const time = (new Date()).getTime()
 /* Customize Function */
 
 const home_page = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/tyrantgenesis.js?t="+time, 'hiker://files/TyrantG/tyrantgenesis.js')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/Home.html?t="+time, 'hiker://files/TyrantG/public/Home.html')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/fonts/AlexBrush-Regular.ttf", 'hiker://files/TyrantG/public/fonts/AlexBrush-Regular.ttf')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
+  godWriteFile(BASE_URL+"tyrantgenesis.js?t="+time, 'hiker://files/TyrantG/tyrantgenesis.js')
+  godWriteFile(BASE_URL+"public/Home.html?t="+time, 'hiker://files/TyrantG/public/Home.html')
+  godSaveFile(BASE_URL+"public/fonts/AlexBrush-Regular.ttf", 'hiker://files/TyrantG/public/fonts/AlexBrush-Regular.ttf')
+  godSaveFile(BASE_URL+"public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
+  godSaveFile(BASE_URL+"public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
 }
 
 const new_homepage = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/HOME/index.html?t="+time, 'hiker://files/TyrantG/HOME/index.html')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TEST/new-homepage.js?t="+time, 'hiker://files/TyrantG/TEST/new-homepage.js')
+  godWriteFile(BASE_URL+"HOME/index.html?t="+time, 'hiker://files/TyrantG/HOME/index.html')
+  godWriteFile(BASE_URL+"TEST/new-homepage.js?t="+time, 'hiker://files/TyrantG/TEST/new-homepage.js')
 }
 
 const tyrantgenesis = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/base.json?t="+time, 'hiker://files/TyrantG/data/base.json')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/tyrantgenesis.js?t="+time, 'hiker://files/TyrantG/tyrantgenesis.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
+  godWriteFile(BASE_URL+"data/base.json?t="+time, 'hiker://files/TyrantG/data/base.json')
+  godWriteFile(BASE_URL+"tyrantgenesis.js?t="+time, 'hiker://files/TyrantG/tyrantgenesis.js')
+  godSaveFile(BASE_URL+"public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
+  godSaveFile(BASE_URL+"public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
+  godSaveFile(BASE_URL+"public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
 }
 
 const acfun = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TEST/acfun-bangumilist.js?t="+time, 'hiker://files/TyrantG/TEST/acfun-bangumilist.js')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/category.html?t="+time, 'hiker://files/TyrantG/public/category.html')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
+  godWriteFile(BASE_URL+"TEST/acfun-bangumilist.js?t="+time, 'hiker://files/TyrantG/TEST/acfun-bangumilist.js')
+  godWriteFile(BASE_URL+"public/category.html?t="+time, 'hiker://files/TyrantG/public/category.html')
+  godSaveFile(BASE_URL+"public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
+  godSaveFile(BASE_URL+"public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
 }
 
 const TMusic = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/music_list.json?t="+time, 'hiker://files/TyrantG/data/music_list.json')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/cloud-music.js?t="+time, 'hiker://files/TyrantG/TOOL/cloud-music.js')
+  godWriteFile(BASE_URL+"data/music_list.json?t="+time, 'hiker://files/TyrantG/data/music_list.json')
+  godWriteFile(BASE_URL+"TOOL/cloud-music.js?t="+time, 'hiker://files/TyrantG/TOOL/cloud-music.js')
 }
 
 const douyu_live = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/douyu.js?t="+time, 'hiker://files/TyrantG/LIVE/douyu.js')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/douyu-tabs.html?t="+time, 'hiker://files/TyrantG/public/douyu-tabs.html')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/douyu-player.html?t="+time, 'hiker://files/TyrantG/public/douyu-player.html')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/css/aliplayer-min.css", 'hiker://files/TyrantG/public/css/aliplayer-min.css')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/aliplayer-min.js", 'hiker://files/TyrantG/public/js/aliplayer-min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
+  godWriteFile(BASE_URL+"LIVE/douyu.js?t="+time, 'hiker://files/TyrantG/LIVE/douyu.js')
+  godWriteFile(BASE_URL+"public/douyu-tabs.html?t="+time, 'hiker://files/TyrantG/public/douyu-tabs.html')
+  godWriteFile(BASE_URL+"public/douyu-player.html?t="+time, 'hiker://files/TyrantG/public/douyu-player.html')
+  godSaveFile(BASE_URL+"public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
+  godSaveFile(BASE_URL+"public/css/aliplayer-min.css", 'hiker://files/TyrantG/public/css/aliplayer-min.css')
+  godSaveFile(BASE_URL+"public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
+  godSaveFile(BASE_URL+"public/js/aliplayer-min.js", 'hiker://files/TyrantG/public/js/aliplayer-min.js')
+  godSaveFile(BASE_URL+"public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
 }
 
 const twitch = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/twitch.js?t="+time, 'hiker://files/TyrantG/LIVE/twitch.js')
+  godWriteFile(BASE_URL+"LIVE/twitch.js?t="+time, 'hiker://files/TyrantG/LIVE/twitch.js')
 }
 
 const _17live = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/17live.js?t="+time, 'hiker://files/TyrantG/LIVE/17live.js')
+  godWriteFile(BASE_URL+"LIVE/17live.js?t="+time, 'hiker://files/TyrantG/LIVE/17live.js')
 }
 
 const huya_live = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/huya.js?t="+time, 'hiker://files/TyrantG/LIVE/huya.js')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/huya-tabs.html?t="+time, 'hiker://files/TyrantG/public/huya-tabs.html')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/huya-category.html?t="+time, 'hiker://files/TyrantG/public/huya-category.html')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/huya-player.html?t="+time, 'hiker://files/TyrantG/public/huya-player.html')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/utils/huya_lib.js", 'hiker://files/TyrantG/public/utils/huya_lib.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/css/aliplayer-min.css", 'hiker://files/TyrantG/public/css/aliplayer-min.css')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/aliplayer-min.js", 'hiker://files/TyrantG/public/js/aliplayer-min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
+  godWriteFile(BASE_URL+"LIVE/huya.js?t="+time, 'hiker://files/TyrantG/LIVE/huya.js')
+  godWriteFile(BASE_URL+"public/huya-tabs.html?t="+time, 'hiker://files/TyrantG/public/huya-tabs.html')
+  godWriteFile(BASE_URL+"public/huya-category.html?t="+time, 'hiker://files/TyrantG/public/huya-category.html')
+  godWriteFile(BASE_URL+"public/huya-player.html?t="+time, 'hiker://files/TyrantG/public/huya-player.html')
+  godSaveFile(BASE_URL+"public/utils/huya_lib.js", 'hiker://files/TyrantG/public/utils/huya_lib.js')
+  godSaveFile(BASE_URL+"public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
+  godSaveFile(BASE_URL+"public/css/aliplayer-min.css", 'hiker://files/TyrantG/public/css/aliplayer-min.css')
+  godSaveFile(BASE_URL+"public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
+  godSaveFile(BASE_URL+"public/js/aliplayer-min.js", 'hiker://files/TyrantG/public/js/aliplayer-min.js')
+  godSaveFile(BASE_URL+"public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
 }
 
 const qie_egame_live = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/qie_egame.js?t="+time, 'hiker://files/TyrantG/LIVE/qie_egame.js')
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/qieEgame-tabs.html?t="+time, 'hiker://files/TyrantG/public/qieEgame-tabs.html')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
-  godSaveFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
+  godWriteFile(BASE_URL+"LIVE/qie_egame.js?t="+time, 'hiker://files/TyrantG/LIVE/qie_egame.js')
+  godWriteFile(BASE_URL+"public/qieEgame-tabs.html?t="+time, 'hiker://files/TyrantG/public/qieEgame-tabs.html')
+  godSaveFile(BASE_URL+"public/css/element-ui.css", 'hiker://files/TyrantG/public/css/element-ui.css')
+  godSaveFile(BASE_URL+"public/js/vue.min.js", 'hiker://files/TyrantG/public/js/vue.min.js')
+  godSaveFile(BASE_URL+"public/js/element-ui.js", 'hiker://files/TyrantG/public/js/element-ui.js')
 }
 
 const qie_sport_live = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/qie_sport.js?t="+time, 'hiker://files/TyrantG/LIVE/qie_sport.js')
+  godWriteFile(BASE_URL+"LIVE/qie_sport.js?t="+time, 'hiker://files/TyrantG/LIVE/qie_sport.js')
 }
 
 const _95_xiu_live = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/LIVE/95_xiu.js?t="+time, 'hiker://files/TyrantG/LIVE/95_xiu.js')
+  godWriteFile(BASE_URL+"LIVE/95_xiu.js?t="+time, 'hiker://files/TyrantG/LIVE/95_xiu.js')
 }
 
 const yande_re = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/yande.re.js?t="+time, 'hiker://files/TyrantG/GHS/yande.re.js')
+  godWriteFile(BASE_URL+"GHS/yande.re.js?t="+time, 'hiker://files/TyrantG/GHS/yande.re.js')
 }
 
 const pornhub = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/pornhub.js?t="+time, 'hiker://files/TyrantG/GHS/pornhub.js')
+  godWriteFile(BASE_URL+"GHS/pornhub.js?t="+time, 'hiker://files/TyrantG/GHS/pornhub.js')
 }
 
 const jable = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/jable.js?t="+time, 'hiker://files/TyrantG/GHS/jable.js')
+  godWriteFile(BASE_URL+"GHS/jable.js?t="+time, 'hiker://files/TyrantG/GHS/jable.js')
 }
 
 const spankbang = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/spankbang.js?t="+time, 'hiker://files/TyrantG/GHS/spankbang.js')
+  godWriteFile(BASE_URL+"GHS/spankbang.js?t="+time, 'hiker://files/TyrantG/GHS/spankbang.js')
 }
 
 const eroticmovies_xyz = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/eroticmovies.xyz.js?t="+time, 'hiker://files/TyrantG/GHS/eroticmovies.xyz.js')
+  godWriteFile(BASE_URL+"GHS/eroticmovies.xyz.js?t="+time, 'hiker://files/TyrantG/GHS/eroticmovies.xyz.js')
 }
 
 const eporner = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/eporner.js?t="+time, 'hiker://files/TyrantG/GHS/eporner.js')
+  godWriteFile(BASE_URL+"GHS/eporner.js?t="+time, 'hiker://files/TyrantG/GHS/eporner.js')
 }
 
 const _7719 = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/7719.js?t="+time, 'hiker://files/TyrantG/GHS/7719.js')
+  godWriteFile(BASE_URL+"GHS/7719.js?t="+time, 'hiker://files/TyrantG/GHS/7719.js')
 }
 
 const shuiguopai = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/shuiguopai.js?t="+time, 'hiker://files/TyrantG/GHS/shuiguopai.js')
+  godWriteFile(BASE_URL+"GHS/shuiguopai.js?t="+time, 'hiker://files/TyrantG/GHS/shuiguopai.js')
 }
 
 const ohentai = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/GHS/ohentai.js?t="+time, 'hiker://files/TyrantG/GHS/ohentai.js')
+  godWriteFile(BASE_URL+"GHS/ohentai.js?t="+time, 'hiker://files/TyrantG/GHS/ohentai.js')
 }
 
 const aktool = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/aktools.js?t="+time, 'hiker://files/TyrantG/TOOL/aktool.js')
+  godWriteFile(BASE_URL+"TOOL/aktools.js?t="+time, 'hiker://files/TyrantG/TOOL/aktool.js')
 }
 
 const prts = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/prts.js?t="+time, 'hiker://files/TyrantG/TOOL/prts.js')
+  godWriteFile(BASE_URL+"TOOL/prts.js?t="+time, 'hiker://files/TyrantG/TOOL/prts.js')
 }
 
 const ys_map = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/ys_map.js?t="+time, 'hiker://files/TyrantG/TOOL/ys_map.js')
+  godWriteFile(BASE_URL+"TOOL/ys_map.js?t="+time, 'hiker://files/TyrantG/TOOL/ys_map.js')
 }
 
 const zczc = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TOOL/zczc.js?t="+time, 'hiker://files/TyrantG/TOOL/zczc.js')
+  godWriteFile(BASE_URL+"TOOL/zczc.js?t="+time, 'hiker://files/TyrantG/TOOL/zczc.js')
 }
 
 const artStation = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/IMAGE/artstation.js?t="+time, 'hiker://files/TyrantG/IMAGE/artstation.js')
+  godWriteFile(BASE_URL+"IMAGE/artstation.js?t="+time, 'hiker://files/TyrantG/IMAGE/artstation.js')
 }
 
 const ednovas = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/VIDEO/ednovas.js?t="+time, 'hiker://files/TyrantG/VIDEO/ednovas.js')
+  godWriteFile(BASE_URL+"VIDEO/ednovas.js?t="+time, 'hiker://files/TyrantG/VIDEO/ednovas.js')
 }
 
 const youtube = _ => {
-  godWriteFile("https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/TEST/youtube.js?t="+time, 'hiker://files/TyrantG/TEST/youtube.js')
+  godWriteFile(BASE_URL+"TEST/youtube.js?t="+time, 'hiker://files/TyrantG/TEST/youtube.js')
+}
+
+const douyin_web = _ => {
+  godWriteFile(BASE_URL+"VIDEO/douyin_web.js?t="+time, 'hiker://files/TyrantG/VIDEO/douyin_web.js')
+  godSaveFile(BASE_URL+"public/js/_signature.js", 'hiker://files/TyrantG/public/js/_signature.js')
 }
 
 /* Customize Function */
---
+++ VIDEO/douyin_web.js
@@ -0,0 +1,39 @@
+const baseParse = _ => {
+    let d = [];
+    const html = getResCode()
+    const category = parseDomForArray(html, '._92400026d1182d4f8f006dada62ebc1c-scss&&a')
+
+    const channel_select = getVar("tyrantgenesis.douyin_web.channel_select", "")
+
+    d.push({
+        title: channel_select === '' ? '‘‘’’<strong><font color="red">全部</font></strong>' : '全部',
+        url: $("hiker://empty").lazyRule(_ => {
+            putVar("tyrantgenesis.douyin_web.channel_select", "")
+            refreshPage(false)
+            return "hiker://empty"
+        }),
+        col_type: 'scroll_button',
+    })
+    category.forEach(cate => {
+        let href = parseDomForHtml(cate, 'a&&href')
+        let channel = href.split('/').pop()
+        if (/^[\d]+$/.test(channel)) {
+            d.push({
+                title: channel_select === channel.toString() ? '‘‘’’<strong><font color="red">'+parseDomForHtml(cate, 'a&&Text')+'</font></strong>' : parseDomForHtml(cate, 'a&&Text'),
+                url: $("hiker://empty").lazyRule(params => {
+                    putVar("tyrantgenesis.douyin_web.channel_select", params.channel.toString())
+                    refreshPage(false)
+                    return "hiker://empty"
+                }, {
+                    channel: channel
+                }),
+                col_type: 'scroll_button',
+            })
+        }
+    })
+
+    const not_sign_url = "https://www.douyin.com/aweme/v1/web/channel/feed/?device_platform=webapp&aid=6383&channel=channel_pc_web&tag_id="+channel_select+"&count=10&version_code=160100&version_name=16.1.0"
+    eval()
+
+    setResult(d);
+}
