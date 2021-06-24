f051dad9df115f1b463e0270c06528755ec2078c
tyrantgenesis <genesis489449701@gmail.com>
2021-06-24 21:56:00 +0800

    dy

--- VIDEO/douyin_web.js
+++ VIDEO/douyin_web.js
@@ -141,23 +141,24 @@ const baseParse = _ => {
                     }),
                     col_type: 'scroll_button',
                 })
-                category.forEach(cate => {
-                    let href = parseDomForHtml(cate, 'a&&href')
-                    let cate_id = href.split('/').pop()
-                    if (/^[\d]+$/.test(cate_id)) {
-                        d.push({
-                            title: cate_select === cate_id.toString() ? '‘‘’’<strong><font color="red">'+parseDomForHtml(cate, 'a&&Text')+'</font></strong>' : parseDomForHtml(cate, 'a&&Text'),
-                            url: $("hiker://empty").lazyRule(params => {
-                                putVar("tyrantgenesis.douyin_web.cate_select", params.cate_id.toString())
-                                refreshPage(false)
-                                return "hiker://empty"
-                            }, {
-                                cate_id: cate_id
-                            }),
-                            col_type: 'scroll_button',
-                        })
-                    }
-                })
+                setError(category)
+                // category.forEach(cate => {
+                //     let href = parseDomForHtml(cate, 'a&&href')
+                //     let cate_id = href.split('/').pop()
+                //     if (/^[\d]+$/.test(cate_id)) {
+                //         d.push({
+                //             title: cate_select === cate_id.toString() ? '‘‘’’<strong><font color="red">'+parseDomForHtml(cate, 'a&&Text')+'</font></strong>' : parseDomForHtml(cate, 'a&&Text'),
+                //             url: $("hiker://empty").lazyRule(params => {
+                //                 putVar("tyrantgenesis.douyin_web.cate_select", params.cate_id.toString())
+                //                 refreshPage(false)
+                //                 return "hiker://empty"
+                //             }, {
+                //                 cate_id: cate_id
+                //             }),
+                //             col_type: 'scroll_button',
+                //         })
+                //     }
+                // })
                 let not_sign_url = "https://www.douyin.com/aweme/v1/web/channel/feed/?device_platform=webapp&aid=6383&channel=channel_pc_web&tag_id="+cate_select+"&count=20&version_code=160100&version_name=16.1.0"
                 let sign = fetch("http://douyin_signature.dev.tyrantg.com?url="+encodeURIComponent(not_sign_url))
                 let true_url = not_sign_url + "&_signature="+sign
@@ -238,17 +239,21 @@ const baseParse = _ => {
                 })
             } else {
                 let data = JSON.parse(data_json)
-                putVar("tyrantgenesis.douyin_web.max_cursor", data.max_cursor.toString())
                 let list = JSON.parse(data_json).aweme_list
-                list.forEach(item => {
-                    d.push({
-                        title: item.desc,
-                        pic_url: item.video.cover.url_list.shift(),
-                        desc: '',
-                        url: item.video.play_addr.url_list.shift() + "#isVideo=true#",
-                        col_type: 'movie_2',
+
+                if (list.length > 0) {
+                    putVar("tyrantgenesis.douyin_web.max_cursor", data.max_cursor.toString())
+
+                    list.forEach(item => {
+                        d.push({
+                            title: item.desc,
+                            pic_url: item.video.cover.url_list.shift(),
+                            desc: '',
+                            url: item.video.play_addr.url_list.shift() + "#isVideo=true#",
+                            col_type: 'movie_2',
+                        })
                     })
-                })
+                }
             }
             break
 
