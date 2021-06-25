8d575384dca874c98fd6e38b9901841aeb9657d7
tyrantgenesis <genesis489449701@gmail.com>
2021-06-25 11:24:14 +0800

    update

--- VIDEO/douyin_web.js
+++ VIDEO/douyin_web.js
@@ -689,7 +689,7 @@ const videoParse = aweme => {
     } else if (aweme.video.play_addr_lowbr) {
         aweme.video.play_addr_lowbr.url_list.forEach((url, index) => {
             d.push({
-                title: "官方线路"+(parseInt(index)+1).toString(),
+                title: "官方线路"+(index+1),
                 url: url+"#isVideo=true#",
                 col_type: "text_4"
             })
