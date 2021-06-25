734b01c0914ee4842e6c27445dff5481a17a6895
tyrantgenesis <genesis489449701@gmail.com>
2021-06-25 15:11:20 +0800

    update

--- VIDEO/douyin_web.js
+++ VIDEO/douyin_web.js
@@ -63,6 +63,7 @@ const baseParse = _ => {
             level_1_button_4_title = '取消关注'
             level_1_button_3_show = '3'
             level_1_button_4_show = '5'
+            channel_prefix = ''
             break
         }
         case "3": {
