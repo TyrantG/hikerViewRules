2222c48f3a42dfa83a03a65cf17088353c423494
tyrantgenesis <genesis489449701@gmail.com>
2021-08-17 16:21:10 +0800

    13

--- TOOL/naraka_score.js
+++ TOOL/naraka_score.js
@@ -2,94 +2,107 @@ const baseParse = _ => {
     const role_path = "hiker://files/rules/js/TyrantGenesis_永劫无间绑定.js"
     let d = [];
     let roleId = ''
+    let empty = 'hiker://empty'
 
     let current_page = MY_URL.split('##')[1].toString()
 
+
+    let cate_select = getVar("tyrantgenesis.naraka_score.cate_select", "1")
+
     if (current_page === '1') {
         d.push({
-            title: '绑定',
+            title: '查询',
             desc: '请输入游戏ID',
             url: "input.trim() ? $('hiker://empty').lazyRule(params => {eval(fetch('hiker://files/TyrantG/TOOL/naraka_score.js'));return setRoleId(params);}, {input: input.trim()}) : 'toast://请输入游戏ID'",
             col_type: "input"
+        })
+        d.push({
+            title: cate_select === '1' ? '‘‘’’<strong><font color="red">最近战绩</font></strong>' : '最近战绩',
+            url: $(empty).lazyRule(_ => {
+                putVar("tyrantgenesis.naraka_score.cate_select", "1")
+                refreshPage(true)
+                return "hiker://empty"
+            }),
+            col_type: 'scroll_button',
+        })
+        d.push({
+            title: cate_select === '2' ? '‘‘’’<strong><font color="red">生涯数据</font></strong>' : '生涯数据',
+            url: $(empty).lazyRule(_ => {
+                putVar("tyrantgenesis.naraka_score.cate_select", "2")
+                refreshPage(true)
+                return "hiker://empty"
+            }),
+            col_type: 'scroll_button',
         })
     }
 
     if (fetch(role_path)) {
         roleId = fetch(role_path)
-        if (current_page === '1') {
-            const channel_select = getVar("tyrantgenesis.youtube.channel_select", "0")
 
+        if (cate_select === '1') {
+            let listData = "https://gamedb.gamersky.com/yjwujian/record/getRecentRecords?roleId="+roleId+"&pageIndex="+current_page+"&pageSize=20"
+            let res_json = fetch(listData)
+            let res = JSON.parse(res_json)
+            if (res.code === 0) {
+                res.data.list.forEach(item => {
+                    d.push({
+                        title: "‘‘’’<strong>#&nbsp;<font color=\"red\">"+item.groupRank+"</font></strong>",
+                        url: "hiker://empty",
+                        col_type: 'text_4',
+                    })
+                    d.push({
+                        title: item.battleMode,
+                        pic_url: "https:"+item.battleModeImage+"@Referer=https://www.gamersky.com/",
+                        url: "hiker://empty",
+                        col_type: 'icon_4',
+                    })
+                    d.push({
+                        title: item.hero,
+                        pic_url: "https:"+item.heroImage+"@Referer=https://www.gamersky.com/",
+                        url: "hiker://empty",
+                        col_type: 'icon_4',
+                    })
+                    d.push({
+                        title: item.grade,
+                        pic_url: "https:"+item.gradeImageUrl+"@Referer=https://www.gamersky.com/",
+                        url: "hiker://empty",
+                        col_type: 'icon_4',
+                    })
+                    d.push({
+                        title: "‘‘’’<strong>分数：&nbsp;<font color=\"red\">"+item.gradeScore+"</font></strong>",
+                        url: "hiker://empty",
+                        col_type: 'text_2',
+                    })
+                    d.push({
+                        title: "‘‘’’<strong>击杀：&nbsp;<font color=\"red\">"+item.killTimes+"</font></strong>",
+                        url: "hiker://empty",
+                        col_type: 'text_2',
+                    })
+                    d.push({
+                        title: "‘‘’’<strong>伤害：&nbsp;<font color=\"red\">"+item.damage+"</font></strong>",
+                        url: "hiker://empty",
+                        col_type: 'text_2',
+                    })
+                    d.push({
+                        title: "‘‘’’<strong>评分：&nbsp;<font color=\"red\">"+item.performScore+"</font></strong>",
+                        url: "hiker://empty",
+                        col_type: 'text_2',
+                    })
+                    d.push({
+                        col_type: 'line_blank'
+                    })
+                })
+            } else {
+                d.push({
+                    title: res.message,
+                    col_type: 'long_text',
+                })
+            }
+        } else if (cate_select === '2') {
             let careerData = "https://gamedb.gamersky.com/yjwujian/career/getCareerData?gamerskyId=5861820&identity=0&roleId="+roleId
-
-            d.push({
-                title: "最近战绩",
-                col_type: "text_1",
-            })
         }
 
-        let listData = "https://gamedb.gamersky.com/yjwujian/record/getRecentRecords?roleId="+roleId+"&pageIndex="+current_page+"&pageSize=20"
-        let res_json = fetch(listData)
-        let res = JSON.parse(res_json)
-        if (res.code === 0) {
-            res.data.list.forEach(item => {
-                d.push({
-                    title: "‘‘’’<strong>#&nbsp;<font color=\"red\">"+item.groupRank+"</font></strong>",
-                    url: "hiker://empty",
-                    col_type: 'text_4',
-                })
-                // d.push({
-                //     title: item.map,
-                //     url: "hiker://empty",
-                //     col_type: 'text_4',
-                // })
-                d.push({
-                    title: item.battleMode,
-                    pic_url: "https:"+item.battleModeImage+"@Referer=https://www.gamersky.com/",
-                    url: "hiker://empty",
-                    col_type: 'icon_4',
-                })
-                d.push({
-                    title: item.hero,
-                    pic_url: "https:"+item.heroImage+"@Referer=https://www.gamersky.com/",
-                    url: "hiker://empty",
-                    col_type: 'icon_4',
-                })
-                d.push({
-                    title: item.grade,
-                    pic_url: "https:"+item.gradeImageUrl+"@Referer=https://www.gamersky.com/",
-                    url: "hiker://empty",
-                    col_type: 'icon_4',
-                })
-                d.push({
-                    title: "‘‘’’<strong>分数：&nbsp;<font color=\"red\">"+item.gradeScore+"</font></strong>",
-                    url: "hiker://empty",
-                    col_type: 'text_2',
-                })
-                d.push({
-                    title: "‘‘’’<strong>击杀：&nbsp;<font color=\"red\">"+item.killTimes+"</font></strong>",
-                    url: "hiker://empty",
-                    col_type: 'text_2',
-                })
-                d.push({
-                    title: "‘‘’’<strong>伤害：&nbsp;<font color=\"red\">"+item.damage+"</font></strong>",
-                    url: "hiker://empty",
-                    col_type: 'text_2',
-                })
-                d.push({
-                    title: "‘‘’’<strong>评分：&nbsp;<font color=\"red\">"+item.performScore+"</font></strong>",
-                    url: "hiker://empty",
-                    col_type: 'text_2',
-                })
-                d.push({
-                    col_type: 'line_blank'
-                })
-            })
-        } else {
-            d.push({
-                title: res.message,
-                col_type: 'long_text',
-            })
-        }
+
 
     }
 
