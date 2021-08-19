c5e33e50940039066cbb39f3318524e47f48a50e
tyrantgenesis <genesis489449701@gmail.com>
2021-08-19 12:00:27 +0800

    13

--- IMAGE/zcool.js
+++ IMAGE/zcool.js
@@ -1,7 +1,7 @@
 const baseParse = _ => {
     // 初始化
     let d = [];
-    const base_url = "https://www.zcool.com.cn/discover"
+    const cate_url = "https://www.zcool.com.cn/common/category"
     const empty = "hiker://empty"
     let api_url = MY_URL.split('##')[0]
     let page = MY_URL.split('##')[1]
@@ -12,21 +12,20 @@ const baseParse = _ => {
     let cate_sort = getVar("tyrantgenesis.zcool.cate_sort", "0")
 
     // 一级分类
-    const html = fetch(base_url, {headers:{"User-Agent": PC_UA}})
-    const cate_array = parseDomForArray(html, '.swiper-wrapper&&.cate-slide');
+    const category_json = fetch(cate_url)
+    const category = JSON.parse(category_json).data
 
-    cate_array.forEach(cate => {
-        let ori_title = parseDomForHtml(cate, 'span&&Text')
-        let cate_id = parseDomForHtml(cate, '.cate-slide&&data-cateid')
-        let title = cate_id.toString() === cate_1st ? '‘‘’’<strong><font color="red">'+ori_title+'</font></strong>' : ori_title
+    // 一级分类
+    category.forEach(cate => {
         d.push({
-            title: title,
+            title: cate_1st === cate.id.toString() ? '‘‘’’<strong><font color="red">'+cate.name+'</font></strong>' : cate.name,
             url: $(empty).lazyRule(params => {
                 putVar("tyrantgenesis.zcool.cate_1st_select", params.cate_id.toString())
+                putVar("tyrantgenesis.zcool.cate_2nd_select", "0")
                 refreshPage(true)
                 return "hiker://empty"
             }, {
-                cate_id: cate_id
+                cate_id: cate.id,
             }),
             col_type: 'scroll_button'
         })
@@ -34,6 +33,35 @@ const baseParse = _ => {
     d.push({
         col_type: 'blank_block',
     })
+
+    // 二级分类
+    let sub_category = category.find(item => item.id.toString() === cate_1st).subCateList
+    d.push({
+        title: cate_2nd === "0" ? '‘‘’’<strong><font color="red">全部</font></strong>' : '全部',
+        url: $(empty).lazyRule(params => {
+            putVar("tyrantgenesis.zcool.cate_2nd_select", "0")
+            refreshPage(true)
+            return "hiker://empty"
+        }),
+        col_type: 'scroll_button'
+    })
+    sub_category.forEach(cate => {
+        d.push({
+            title: cate_2nd === cate.id.toString() ? '‘‘’’<strong><font color="red">'+cate.name+'</font></strong>' : cate.name,
+            url: $(empty).lazyRule(params => {
+                putVar("tyrantgenesis.zcool.cate_2nd_select", params.cate_id.toString())
+                refreshPage(true)
+                return "hiker://empty"
+            }, {
+                cate_id: cate.id
+            }),
+            col_type: 'scroll_button'
+        })
+    })
+    d.push({
+        col_type: 'blank_block',
+    })
+
 
     setResult(d);
 }
