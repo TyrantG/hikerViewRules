aaa13ba7fd342e7942807bc3291c355530cb191d
tyrantgenesis <genesis489449701@gmail.com>
2021-09-04 14:13:05 +0800

    sign

--- VIDEO/douyin_web.js
+++ VIDEO/douyin_web.js
@@ -263,7 +263,7 @@ const baseParse = _ => {
                 })
             } else {
                 let list = JSON.parse(data_json).aweme_list
-                setError(list[9])
+                setError(list[9].video)
                 if (list && list.length > 0) {
                     list.forEach(item => {
                         if (item.video && item.author) {
