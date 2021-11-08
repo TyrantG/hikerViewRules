2661871913ac97b279e3b0e3b70382f84c6b9cdd
tyrantgenesis <genesis489449701@gmail.com>
2021-11-08 13:44:14 +0800

    123

--- COLLECTION/gods_plan.js
+++ COLLECTION/gods_plan.js
@@ -1,7 +1,6 @@
 // const LOCAL_ASSETS = 'hiker://files/rules/TyrantG/assets.js'
 const FETCH_FILE = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/COLLECTION/gods_plan.js'
 const VERSION_FILE = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/version'
-const ASSET_PATH = 'hiker://files/rules/TyrantG/assets/'
 
 const VERSION = '1.0.0-beta'
 
@@ -10,22 +9,26 @@ const baseParse = _ => {
     log(fileExist(ASSET_PATH+'images/avatar.jpg'))
 
     if (VERSION !== request(VERSION_FILE)) {
-        syncTitle = "God's Plan可更新.当前版本"+VERSION+'.最新版本: '+request(VERSION_FILE)
+        syncTitle = "God's Plan可更新 - 最新版本: "+request(VERSION_FILE)
     }
 
     d.push({
         title: syncTitle,
-        image_url: ASSET_PATH+'images/avatar.jpg',
+        image_url: "https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/assets/images/avatar.jpg",
         url: $().lazyRule(_ => {
             const LOCAL_FILE = 'hiker://files/rules/TyrantG/gods_plan.js'
             const FETCH_FILE = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/COLLECTION/gods_plan.js'
-            const FETCH_ASSETS = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/COLLECTION/assets.js'
+            // const FETCH_ASSETS = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/COLLECTION/assets.js'
             writeFile(LOCAL_FILE, request(FETCH_FILE))
-            eval(request(FETCH_ASSETS))
+            // eval(request(FETCH_ASSETS))
             refreshPage(false)
             return "hiker://empty"
         }),
         col_type: 'avatar'
+    })
+
+    d.push({
+        col_type: 'input'
     })
 
     setResult(d);
