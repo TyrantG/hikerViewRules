7d838e71d54a01e80c200fd8cf5dcef74458ed2a
tyrantgenesis <genesis489449701@gmail.com>
2021-11-08 10:54:43 +0800

    123

--- COLLECTION/gods_plan.js
+++ COLLECTION/gods_plan.js
@@ -1,5 +1,3 @@
-const LOCAL_FILE = 'hiker://files/rules/TyrantG/gods_plan.js'
-const FETCH_FILE = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/COLLECTION/gods_plan.js'
 const VERSION_FILE = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/version'
 
 const VERSION = '1.0.0-beta'
