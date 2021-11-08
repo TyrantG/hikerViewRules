9f1342eac653149dea318320bd135d95d9aa368a
tyrantgenesis <genesis489449701@gmail.com>
2021-11-08 11:57:42 +0800

    123

--- COLLECTION/assets.js
+++ COLLECTION/assets.js
@@ -2,8 +2,7 @@ const BASE_URL = "https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/maste
 const LOCAL_URL = 'hiker://files/rules/TyrantG/assets/'
 
 const godSaveFile = (asset_path) => {
-    log(LOCAL_URL+asset_path)
-    writeFile(LOCAL_URL+asset_path, request(BASE_URL+asset_path))
+    saveImage(BASE_URL+asset_path, LOCAL_URL+asset_path)
 }
 
 godSaveFile('images/avatar.jpg')
