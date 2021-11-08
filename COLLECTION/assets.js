d4dae60e13e38aba69ad52f45f600a66033200cf
tyrantgenesis <genesis489449701@gmail.com>
2021-11-08 11:34:24 +0800

    123

---
+++ COLLECTION/assets.js
@@ -0,0 +1,8 @@
+const BASE_URL = "https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/assets/"
+const LOCAL_URL = 'hiker://files/rules/TyrantG/assets/'
+
+const godSaveFile = (asset_path) => {
+  writeFile(local_file+asset_path, request(fetch_file+asset_path))
+}
+
+godSaveFile('images/avatar.jpg')
--- COLLECTION/gods_plan.js
+++ COLLECTION/gods_plan.js
@@ -1,13 +1,32 @@
+const LOCAL_FILE = 'hiker://files/rules/TyrantG/gods_plan.js'
+// const LOCAL_ASSETS = 'hiker://files/rules/TyrantG/assets.js'
+const FETCH_FILE = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/COLLECTION/gods_plan.js'
+const FETCH_ASSETS = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/COLLECTION/assets.js'
+const ASSET_PATH = 'hiker://files/rules/TyrantG/assets/'
 const VERSION_FILE = 'https://git.tyrantg.com/tyrantgenesis/hikerViewRules/raw/master/data/version'
 
 const VERSION = '1.0.0-beta'
 
+const sync = _ => {
+    writeFile(LOCAL_FILE, request(FETCH_FILE))
+    eval(request(FETCH_ASSETS))
+}
+
 const baseParse = _ => {
-    let d = [];
+    let d = [], syncTitle = "God's Plan 当前版本："+VERSION
+
+    if (VERSION !== request(FETCH_FILE)) {
+        syncTitle += '. 最新版本: '+request(VERSION_FILE)
+    }
 
     d.push({
-        title: "God's Plan 当前版本："+VERSION,
-        url: "",
+        title: syncTitle,
+        image_url: request(ASSET_PATH+'images/avatar.jpg'),
+        url: $().lazyRule(_ => {
+            const LOCAL_FILE = 'hiker://files/rules/TyrantG/gods_plan.js'
+            eval(LOCAL_FILE)
+            sync()
+        }),
         col_type: 'avatar'
     })
 
