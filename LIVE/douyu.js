43c978827ad735246179f554a3b4d7118503b508
tyrantgenesis <genesis489449701@gmail.com>
2021-11-09 12:40:01 +0800

    1234

--- LIVE/douyu.js
+++ LIVE/douyu.js
@@ -178,9 +178,9 @@ const cateGroupParse = _ => {
 }
 
 const getSign = (script, rid, did, tt) => {
-    let result = script.match(/vdwdae325w_64we[\s\S]*function ub98484234([\s\S]*?)function/)[0]
+    let result = script.match(/(vdwdae325w_64we[\s\S]*function ub98484234[\s\S]*?)function/)[0]
     setError(result)
-    let func_ub9 = result.replace(/eval.*;}/, 'strc;}', result)
+    /* let func_ub9 = result.replace(/eval.*;}/, 'strc;}', result)
     eval(func_ub9)
 
     let res = ub98484234()
@@ -193,5 +193,5 @@ const getSign = (script, rid, did, tt) => {
     eval(func_sign)
 
     let params = sign(rid, did, tt) + "&cdn=ws-h5&rate=0"
-    return params
+    return params */
 }
