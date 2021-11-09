52e95ae313dbdc3fc031ea510c64ce92f3a1aa1b
tyrantgenesis <genesis489449701@gmail.com>
2021-11-09 15:32:30 +0800

    1234

--- TEXT/juejin.js
+++ TEXT/juejin.js
@@ -1,15 +1,14 @@
 const baseParse = _ => {
     // 初始化
     let d = [];
-    const cate_url = "https://www.zcool.com.cn/common/category"
+    const cate_url = "https://juejin.cn"
     const empty = "hiker://empty"
     const page = MY_URL.split('##')[1]
     const sort = [{title: '编辑精选', url: 'recommendLevel=2&sort=9'},{title: '首页推荐', url: 'recommendLevel=3&sort=9'},{title: '全部推荐', url: 'recommendLevel=1&sort=9'},{title: '最新发布', url: 'recommendLevel=0&sort=0'}]
 
     // 缓存
-    let cate_1st = getVar("tyrantgenesis.zcool.cate_1st_select", "0")
-    let cate_2nd = getVar("tyrantgenesis.zcool.cate_2nd_select", "0")
-    let cate_sort = getVar("tyrantgenesis.zcool.cate_sort", "0")
+    let cate_1st = getVar("tyrantgenesis.juejin.cate_1st_select", "0")
+    let cate_2nd = getVar("tyrantgenesis.juejin.cate_2nd_select", "0")
 
     const api_url = MY_URL.split('##')[0] + "?cate="+cate_1st+"&subCate="+cate_2nd+"&hasVideo=0&city=0&college=0&"+sort[cate_sort].url+"&limit=20&page="+page
 
@@ -98,7 +97,7 @@ const baseParse = _ => {
                     pic_url: obj.cover,
                     url: $(obj.pageUrl).rule(_ => {
                         eval(fetch('hiker://files/TyrantG/IMAGE/zcool.js'))
-                        secParse(MY_URL)
+                        secParse()
                     }),
                     col_type: 'movie_2'
                 })
@@ -109,10 +108,9 @@ const baseParse = _ => {
     setResult(d);
 }
 
-const secParse = MY_URL => {
+const secParse = _ => {
     let d = [];
-    const pageContent = fetch(MY_URL)
-    const html = parseDomForHtml(pageContent, '.work-content-wrap&&Html');
+    const html = parseDomForHtml(getResCode(), '.work-content-wrap&&Html');
 
     d.push({
         title: html.replace(/收藏/g, '').replace(/<script(.|\r|\n)*script>/, ''),
@@ -136,7 +134,7 @@ const searchParse = _ => {
                     pic_url: obj.cover,
                     url: $(obj.pageUrl).rule(_ => {
                         eval(fetch('hiker://files/TyrantG/IMAGE/zcool.js'))
-                        secParse(MY_URL)
+                        secParse()
                     }),
                     col_type: 'movie_2'
                 })
@@ -145,4 +143,101 @@ const searchParse = _ => {
     })
 
     setResult(d);
+}
+
+
+js:
+var myCollection_el = []
+
+// 处理MY_URL和MY_RULE
+let RULE = MY_PARAMS.RULE
+MY_RULE.version = RULE.version
+MY_RULE.group = RULE.group
+MY_RULE.title = RULE.title
+MY_RULE.url = MY_URL
+MY_RULE.ua = RULE.ua
+MY_RULE.col_type = RULE.detail_col_type
+MY_RULE.find_rule = RULE.detail_find_rule
+MY_RULE.preRule = RULE.preRule
+MY_RULE.pageList = RULE.pageList
+MY_RULE.pages = RULE.pages
+MY_RULE.last_chapter_rule = RULE.last_chapter_rule
+MY_RULE.params = RULE.params
+// 解析正文
+if(MY_PARAMS.pageTitle) setPageTitle(MY_PARAMS.pageTitle)
+eval(MY_RULE.preRule)
+setLastChapterRule(MY_RULE.last_chapter_rule)
+MY_PARAMS = MY_RULE.params
+if (MY_RULE.find_rule.startsWith('js:')) {
+    function setResult(el, param1, param2, param3) {
+        param1 = CALLBACK_KEY
+        param2 = MY_RULE
+        param3 = MY_TYPE
+        if (Array.isArray(el.data)) el = el.data
+
+        Array.prototype.push.apply(myCollection_el, el.map((v) => {
+            let RULE = Object.assign({}, MY_RULE)
+            v.col_type = v.col_type || MY_RULE.col_type
+
+            if (!v.url) {
+                return v
+            } else if (v.url.includes('@lazyRule=')) {
+                v.url = v.url.replace('.js:', '.js:\nif(MY_RULE)Object.assign(MY_RULE,' + $.stringify({ title: MY_RULE.title, pages: MY_RULE.pages, pageList: MY_RULE.pageList, find_rule: '', params: '' }) + ');')
+                return v
+            } else if (v.url.includes('@rule=')) {
+                let [_, url, rule] = v.url.match(/^([\s\S]*?)@rule=([\s\S]*)$/)
+                v.url = url
+                RULE.detail_find_rule = rule
+                RULE.detail_col_type = MY_RULE.col_type
+            } else if (v.url.startsWith('hiker://page/')) {
+                if (v.url.includes('rule=') || (v.extra || {}).rule)
+                    return v
+                let path = v.url.split('#')[0].match(/^hiker:\/\/page\/(.+?)(?:\?.*)?$/)[1],
+                    subPage = MY_RULE.pageList.find((v) => v.path == path)
+                RULE.detail_find_rule = subPage.rule
+                RULE.detail_col_type = subPage.col_type
+                RULE.params = v.extra || {}
+            } else { return v }
+            v.extra = { url: v.url, RULE: RULE }
+            v.url = 'hiker://page/detailLoad?rule=' + getItem('myCollection')
+            return v
+        }))
+        method_setResult.invoke(javaContext, myCollection_el, param1, param2, param3)
+    }
+
+    function setHomeResult(a, param1, param2, param3) {
+        setResult(a, param1, param2, param3)
+    }
+
+    function setSearchResult(a, param1, param2, param3) {
+        setResult(a, param1, param2, param3)
+    }
+
+    eval(MY_RULE.find_rule.slice(3))
+} else {
+    const { runCode } = $.require('hiker://page/utility?rule=' + getItem('myCollection'))
+    let findRule = MY_RULE.find_rule.split(';')
+    parseDomForArray(getResCode(), findRule.shift()).forEach((data) => {
+        let [title, img, desc, url] = findRule.map((v, i) => {
+                try {
+                    if (v == '*') return ''
+                    else v = (i == 1 || i == 3) ?
+                        parseDom(data, v) :
+                        parseDomForHtml(data, v)
+                    if (i != 3) v = runCode(v)
+                    return v
+                } catch (e) {}
+            }),
+            res = {
+                title: title,
+                url: url,
+                desc: desc,
+                img: img,
+                col_type: MY_RULE.col_type
+            }
+        if (res.url && res.url.includes('@lazyRule='))
+            res.url = res.url.replace('.js:', '.js:\nif(MY_RULE)Object.assign(MY_RULE,' + $.stringify({ title: MY_RULE.title, pages: MY_RULE.pages, pageList: MY_RULE.pageList, find_rule: '', params: '' }) + ');')
+        myCollection_el.push(res)
+    })
+    setResult(myCollection_el)
 }
