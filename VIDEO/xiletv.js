3e75d077bbcd151b06b1cc00a381b95e681fbb18
tyrantgenesis <genesis489449701@gmail.com>
2021-12-25 14:51:24 +0800

    123

--- VIDEO/xiletv.js
+++ VIDEO/xiletv.js
@@ -1,15 +1,25 @@
 const baseParse = _ => {
     let d = [];
     const current_page = MY_URL.split('##')[1]
-    const current_url = getVar('true_url', MY_URL)
+    let current_url = getVar('true_url', MY_URL)
+
+    if (! current_url) {
+        const ori_html = fetch(MY_URL, {headers: {"User-Agent": PC_UA}})
+        let top_temp = pdfa(ori_html, '.nav&&ul&&li')
+        putVar('true_url', top_temp[1])
+        current_url = top_temp[1]
+    }
+
+    const html = fetch(current_url, {headers: {"User-Agent": PC_UA}})
+    let top_cate = pdfa(ori_html, '.nav&&ul&&li')
+    top_cate.shift().pop()
+
+    log(html)
 
     const fold = getVar("fold", "0")
     const cate_temp_json = getVar("category")
     const cate_temp = cate_temp_json ? JSON.parse(cate_temp_json) : []
 
-    const html = fetch(current_url, {headers: {"User-Agent": PC_UA}})
-    const top_cate = pdfa(html, '.nav&&ul&&li')
-    log(top_cate)
 
     if (parseInt(current_page) === 1) {
 
