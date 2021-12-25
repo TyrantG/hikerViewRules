07757344b3d0f51374382ca0664ce7bd99d8fff5
tyrantgenesis <genesis489449701@gmail.com>
2021-12-25 14:43:12 +0800

    123

--- VIDEO/xiletv.js
+++ VIDEO/xiletv.js
@@ -2,8 +2,18 @@ const baseParse = _ => {
     let d = [];
     const current_page = MY_URL.split('##')[1]
     const current_url = getVar('true_url', MY_URL)
-    log(current_page)
-    log(current_url)
+
+    const fold = getVar("fold", "0")
+    const cate_temp_json = getVar("category")
+    const cate_temp = cate_temp_json ? JSON.parse(cate_temp_json) : []
+
+    const html = fetch(current_url, {headers: {"User-Agent": PC_UA}})
+    const top_cate = pdfa(html, '.nav&&ul&&li')
+    log(top_cate)
+
+    if (parseInt(current_page) === 1) {
+
+    }
 
     setResult(d);
 }
