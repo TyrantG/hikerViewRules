15ddd390d1f4241aa9413cbb261d170e2be7cc66
tyrantgenesis <genesis489449701@gmail.com>
2022-02-12 00:38:07 +0800

    123

--- COMIC/picacg.js
+++ COMIC/picacg.js
@@ -1,6 +1,6 @@
-js:
 const picacg = {
     BaseUrl: "https://picaapi.picacomic.com/",
+    empty: 'hiker://empty',
     picacg_path: "hiker://files/rules/js/TyrantGenesis_哔咔设置.js",
     apiKey: "C69BAF41DA5ABD1FFEDC6D2FEA56B",
     nonce: "b1ab87b4800d4d4590a11701b8551afa",
@@ -28,6 +28,7 @@ const picacg = {
         return CryptoJS.HmacSHA256(raw, picacg.secretKey).toString(CryptoJS.enc.Hex)
     },
     httpRequest: (path, method, data) => {
+        let responseJson
         const request_url = picacg.BaseUrl + path
         // const time = (new Date().getTime()/1000).toFixed(0)
         let headers = picacg.headers
@@ -36,18 +37,34 @@ const picacg = {
         headers.nonce = picacg.nonce
         headers.signature = picacg.encrypt(request_url, headers.time, method)
 
-        if (method === 'GET')
-            return fetch(request_url, {headers: headers})
-        else
-            return fetch(request_url, {headers: headers, method: method, body: data})
+        try {
+            if (fileExist(picacg.picacg_path)) headers.authorization = fetch(picacg.picacg_path).split('\n')[2]
+        } catch (e) {}
+
+        /*if (method === 'GET')
+            responseJson = fetch(request_url, {headers: headers})
+        else*/
+            responseJson = fetch(request_url, {headers: headers, method: method, body: data})
+
+        return JSON.parse(responseJson)
     },
-    get: path => picacg.httpRequest(path, 'GET', {}),
+    get: (path, data) => picacg.httpRequest(path, 'GET', data),
     post: (path, data) => picacg.httpRequest(path, 'POST', data),
     baseParse: () => {
-        picacg.d.push({
-            title: picacg.login(),
-            col_type: 'long_text'
-        })
+        if (! fileExist(picacg.picacg_path)) {
+            picacg.d.push({
+                title: '登录哔咔',
+                url: $(picacg.empty).lazyRule((picacg_path) => {
+                    writeFile(picacg_path, 'tyrantgenesis\nGenesis489449701\neyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OGQ2ZTI0ZGRhMDU2ZTdmOTdiNmVmNjkiLCJlbWFpbCI6InR5cmFudGdlbmVzaXMiLCJyb2xlIjoibWVtYmVyIiwibmFtZSI6IlR5cmFudEdlbmVzaXMiLCJ2ZXJzaW9uIjoiMi4yLjEuMi4zLjMiLCJidWlsZFZlcnNpb24iOiI0NCIsInBsYXRmb3JtIjoiYW5kcm9pZCIsImlhdCI6MTY0NDU5NDA5MCwiZXhwIjoxNjQ1MTk4ODkwfQ.0XROdah4UyuBBJ03k962biuKZsQfrZQhFcwqAjMd4RQ')
+                    refreshPage(false)
+                    return 'hiker://empty'
+                }, picacg.picacg_path),
+                col_type: 'text_center_1'
+            })
+        } else {
+            picacg.getCategories()
+        }
+
         setResult(picacg.d);
     },
     login: () => {
@@ -55,6 +72,54 @@ const picacg = {
             email: 'tyrantgenesis',
             password: 'Genesis489449701'
         })
+    },
+    getCategories: () => {
+        const response = picacg.get('categories', {})
+        if (response.code === 200) {
+            const except = [''];
+            response.data.categories.forEach((cate, index) => {
+                if (index > 6) {
+                    picacg.d.push({
+                        title: cate.title,
+                        desc: '0',
+                        pic_url: cate.thumb.fileServer+'/static/'+cate.thumb.path,
+                        url: $(picacg.empty+'$$fypage').rule((title) => {
+                            const picacg = $.require('hiker://page/picacg')
+                            picacg.getComics(title)
+                            setResult(picacg.d);
+                        }, cate.title),
+                        col_type: 'card_pic_2'
+                    })
+                }
+            })
+        }
+    },
+    getComics: (title) => {
+        const page = MY_URL.split('$$')[1]
+        const response = picacg.get('comics', {
+            c: title,
+            page: page
+        })
+        // const response = picacg.get('comics?c='+title+'&page='+page, '')
+        log(response)
+        if (response.code === 200) {
+            response.data.comics.docs.forEach((comic, index) => {
+                if (index > 6) {
+                    picacg.d.push({
+                        title: comic.title,
+                        desc: comic.author,
+                        pic_url: comic.thumb.fileServer+'/static/'+comic.thumb.path,
+                        /*url: $(picacg.empty+'$$fypage').rule((title) => {
+                            const picacg = $.require('hiker://page/picacg')
+                            picacg.getComics(title)
+                            setResult(picacg.d);
+                        }, cate.title),*/
+                        col_type: 'movie_2'
+                    })
+                }
+            })
+        }
     }
 }
-picacg.baseParse()
+
+$.exports = picacg
