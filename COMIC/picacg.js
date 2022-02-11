02078bbfc102a838b05cad4d8d1b75350273eecd
tyrantgenesis <genesis489449701@gmail.com>
2022-02-11 16:38:46 +0800

    13

--- COMIC/picacg.js
+++ COMIC/picacg.js
@@ -5,7 +5,7 @@ const picacg = {
     nonce: "b1ab87b4800d4d4590a11701b8551afa",
     secretKey: "~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn",
     d: [],
-    header: {
+    headers: {
         "accept":            "application/vnd.picacomic.com.v1+json",
         "app-channel":       "3",
         // "signature":         "",
