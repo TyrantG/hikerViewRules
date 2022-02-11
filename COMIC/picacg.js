bcd2b90ef636402d003ffe442c06dbc9b444d8f1
tyrantgenesis <genesis489449701@gmail.com>
2022-02-11 16:37:18 +0800

    13

--- COMIC/picacg.js
+++ COMIC/picacg.js
@@ -1,67 +1,50 @@
-const config = {
-    BaseUrl: "http://68.183.234.72/",
-    Url: "https://picaapi.picacomic.com/",
-}
+const picacg = {
+    BaseUrl: "https://picaapi.picacomic.com/",
+    picacg_path: "hiker://files/rules/js/TyrantGenesis_哔咔设置.js",
+    apiKey: "C69BAF41DA5ABD1FFEDC6D2FEA56B",
+    nonce: "b1ab87b4800d4d4590a11701b8551afa",
+    secretKey: "~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn",
+    d: [],
+    header: {
+        "accept":            "application/vnd.picacomic.com.v1+json",
+        "app-channel":       "3",
+        // "signature":         "",
+        "app-version":       "2.2.1.3.3.4",
+        "app-uuid":          "defaultUuid",
+        "app-platform":      "android",
+        "app-build-version": "45",
+        "Content-Type":      "application/json; charset=UTF-8",
+        "User-Agent":        "okhttp/3.8.1",
+        // "authorization":     getConf("authorization"),
+        "image-quality":     "original",
+    },
+    encrypt: (url, ts, method) => {
+        eval(getCryptoJS())
+        let raw = url.replace(picacg.BaseUrl, "") + ts + picacg.nonce + method + picacg.apiKey
+        raw = raw.toLocaleLowerCase()
+        // return crypto.createHmac('sha256', secret_key).update(raw).digest('hex')
+        return CryptoJS.HmacSHA256(raw, picacg.secretKey).toString(CryptoJS.enc.Hex)
+    },
+    httpRequest: (path, method) => {
+        const request_url = picacg.BaseUrl + path
+        let headers = picacg.headers
+        headers['api-key'] = picacg.apiKey
+        headers.time = (new Date().getTime()/1000).toFixed(0)
+        headers.nonce = picacg.nonce
+        headers.signature = picacg.encrypt(request_url, headers.time, method)
 
-let header = {
-    "api-key":           "C69BAF41DA5ABD1FFEDC6D2FEA56B",
-    "accept":            "application/vnd.picacomic.com.v1+json",
-    "app-channel":       "3",
-    "time":              (new Date().getTime()/1000).toString(),
-    "nonce":             "b1ab87b4800d4d4590a11701b8551afa",
-    // "signature":         "",
-    "app-version":       "2.2.1.3.3.4",
-    "app-uuid":          "defaultUuid",
-    "app-platform":      "android",
-    "app-build-version": "45",
-    "Content-Type":      "application/json; charset=UTF-8",
-    "User-Agent":        "okhttp/3.8.1",
-    "authorization":     getConf("authorization"),
-    "image-quality":     "original",
-}
-
-const picacg_path = "hiker://files/rules/js/TyrantGenesis_哔咔设置.js"
-
-let picacg_config = fetch(picacg_path)
-if (! picacg_config) {
-    picacg_config = "username=\r\npassword=\r\nauthorization=\r\n"
-    writeFile(picacg_path, picacg_config)
-}
-// url = config.Url + "auth/sign-in"
-
-const baseParse = _ => {
-    // 初始化
-    let d = [];
-    let username = getConf("username")
-    let password = getConf("password")
-
-    setResult(d);
-}
-
-const getConf = key => {
-    let reg = new RegExp(key+"=(.*?)")
-    const match = config.match(reg, picacg_config)
-    if (match && match[1])
-        return match[1]
-    else
-        return false
-}
-
-const req = (url, data) => {
-    url = config.Url + url
-
-}
-
-const getHeader = (url, method) => {
-    return getSign(header, url, method)
-}
-
-const getSign = (header, url, method) => {
-    eval(getCryptoJS())
-    let raw = (url + header.time + header.nonce + method + header.apiKey).toLowerCase()
-    let h = CryptoJS.HmacSHA256("~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn", )
-    // h := hmac.New(sha256.New, []byte())
-    // h.Write([]byte(raw))
-    // header["signature"] = hex.EncodeToString(h.Sum(nil))
-    // return header
+        return fetch(request_url, {headers: headers})
+    },
+    get: path => picacg.httpRequest(path, 'GET'),
+    post: path => picacg.httpRequest(path, 'POST'),
+    baseParse: () => {
+        picacg.d.push({
+            title: picacg.login(),
+            col_type: 'long_text'
+        })
+        setResult(picacg.d);
+    },
+    login: () => {
+        return picacg.post('auth/sign-in')
+    }
 }
