b835436d61b053fa57f78f5e793bd272bf425d5a
tyrantgenesis <genesis489449701@gmail.com>
2022-10-29 11:31:00 +0800

    YouTube

--- TEST/youtube.js
+++ TEST/youtube.js
@@ -417,7 +417,7 @@ const secParse = params => {
     //
     // d.pop()
 
-    const res_json = fetch('https://s4.save.tube/ajax/getLinks.php?video='+encodeURIComponent(ori_url)+'&rand=FzwXzXGso4g2XeV')
+    const res_json = fetch('https://s2.save.tube/ajax/getLinks.php?video='+encodeURIComponent(ori_url)+'&rand=FzwXzXGso4g2XeV')
     const res = JSON.parse(res_json)
 
     if (res.status === 'success') {
