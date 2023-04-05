29fcd6fd491ee776bdb16d77a0b10d9dd65d8ace
tyrantgenesis <genesis489449701@gmail.com>
2023-04-05 09:52:10 +0800

    jable

--- GHS/jable.js
+++ GHS/jable.js
@@ -1,28 +1,33 @@
 const baseParse = _ => {
   let d = [];
-  const list = parseDomForArray(getResCode(), '.site-content&&.video-img-box');
-  for (let j in list) {
-    d.push({
-      title: parseDomForHtml(list[j], 'h6&&a&&Text'),
-      desc: parseDomForHtml(list[j], '.label&&Text'),
-      pic_url: parseDom(list[j], 'img&&data-src'),
-      url: parseDom(list[j],'a&&href')
-    })
-  }
+  try {
+    const list = parseDomForArray(getResCode(), '.site-content&&.video-img-box');
+    for (let j in list) {
+      d.push({
+        title: parseDomForHtml(list[j], 'h6&&a&&Text'),
+        desc: parseDomForHtml(list[j], '.label&&Text'),
+        pic_url: parseDom(list[j], 'img&&data-src'),
+        url: parseDom(list[j],'a&&href')
+      })
+    }
+  } catch (e){}
+
 
   setResult(d);
 }
 
 const secParse = _ => {
   let d = [];
-  const html = getResCode()
+  try {
+    const html = getResCode()
 
-  d.push({
-    title: parseDomForHtml(html, '.header-left&&h4&&Text'),
-    desc: parseDomForHtml(html, '.video-info&&.text-center&&.tags&&Text'),
-    pic_url: parseDom(html, 'video&&poster'),
-    url: html.match(/hlsUrl = '(.*?)'/)[1]
-  })
+    d.push({
+      title: parseDomForHtml(html, '.header-left&&h4&&Text'),
+      desc: parseDomForHtml(html, '.video-info&&.text-center&&.tags&&Text'),
+      pic_url: parseDom(html, 'video&&poster'),
+      url: html.match(/hlsUrl = '(.*?)'/)[1]
+    })
+  } catch (e){}
 
   setResult(d);
 }
